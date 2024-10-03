5b142d2526812d1d1abc6bf82ecc9c80266694f4
Piccio <lorenzo.hl.picchi@gmail.com>
2024-10-03 12:33:38 +0200

    set a json file filled with path's link to the site's pages

--- portfolio/src/components/MainPage.vue
+++ portfolio/src/components/MainPage.vue
@@ -3,15 +3,15 @@
 </script>
 
 <template>
-  <div id="app">
-    <header>
-        <nav>
-            <ul>
-                <li v-for="link in links" :key="link.path">
-                    <router-link :to="link.path">{{ link.name }}</router-link>
-                </li>
-            </ul>
-        </nav>
-    </header>
-  </div>
+    <div id="app">
+        <header>
+            <nav>
+                <ul>
+                    <li v-for="link in links" :key="link.path">
+                        <router-link :to="link.path">{{ link.name }}</router-link>
+                    </li>
+                </ul>
+            </nav>
+        </header>
+    </div>
 </template>
