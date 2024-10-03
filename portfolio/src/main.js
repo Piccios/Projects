c3097000be3bd4fb738134e1636265818ca82a9e
Piccio <lorenzo.hl.picchi@gmail.com>
2024-10-03 15:20:57 +0200

    imported pages and routes

--- portfolio/src/main.js
+++ portfolio/src/main.js
@@ -1,5 +1,21 @@
-import { createApp } from 'vue'
-import './assets/style.scss'
-import App from './App.vue'
+import { createApp } from 'vue';
+import App from './App.vue';
+import MainPage from './pages/MainPage.vue';
+import Index from './pages/Index.vue';
+import Contacts from './pages/Contacts.vue';
+import { createRouter, createWebHistory } from 'vue-router';
 
-createApp(App).mount('#app')
+const routes = [
+    { path: '/', component: MainPage },
+    { path: '/index', component: Index },
+    { path: '/contacts', component: Contacts }
+];
+
+const router = createRouter({
+    history: createWebHistory(),
+    routes
+});
+
+const app = createApp(App);
+app.use(router);
+app.mount('#app');
